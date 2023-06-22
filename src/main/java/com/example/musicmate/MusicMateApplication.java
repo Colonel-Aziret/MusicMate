7bae595d8a4c5f93cfcead3daa55edd2fb562792
Colonel-Aziret <Aziret5265@gmail.com>
2023-06-22 10:40:15 +0600

    Initial commit

---
+++ src/main/java/com/example/musicmate/MusicMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musicmate/MusicMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
